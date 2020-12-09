3d13f6653033228c35be81e0eb9f1e79a651cbd2
ddm0 <70338737+ddm0@users.noreply.github.com>
2020-12-09 00:21:47 -0400

    Update server and client structure

--- Server.java
+++ Server.java
@@ -4,40 +4,101 @@ import java.net.InetAddress;
 import java.io.IOException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import javafx.application.Platform; 
 
-public class Server extends Listenable {
-	private InetAddress ip;
+public class Server extends Thread {
+	private InetAddress addr;
 	private int port;
 	private Socket socket;
 	private ServerSocket serversocket;
-	private DataInputStream input;
-	private DataOutputStream output;
+	private DataInputStream input; private DataOutputStream output;
+	private boolean running = false;
+	private GUI gui;
 
-	Server(String ip, int port) throws IOException {
-		InetAddress addr;
+	public String print;
 
+	Server() {}
+
+	Server(String ip, int port, GUI gui) throws IOException {
 		if (ip != null && !ip.isEmpty()) {
 			addr = InetAddress.getByName(ip);
 		} else {
 			addr = InetAddress.getLocalHost();
 		}
-
-		serversocket = new ServerSocket(port, 1, addr);
+		this.port = port;
+		this.gui = gui;
 	}
 
-	void stop() throws IOException {
+	void open() throws IOException {
+		serversocket = new ServerSocket(port, 1, addr); 
+		start();
+	}
+
+	void close() throws IOException {
+		running = false;
 		serversocket.close();
 	}
 
 	void sendData(String data) throws IOException { 
-		output.writeChars(data);
+		output.writeUTF(data);
 	}
 	
-	String listen() throws IOException {
-		socket = serversocket.accept();
-		output = new DataOutputStream(socket.getOutputStream());
-		input = new DataInputStream(socket.getInputStream());
-		return input.readUTF();
+	public void run() {
+		running = true;
+
+		while (running) {
+			try {
+				socket = serversocket.accept();
+				output = new DataOutputStream(socket.getOutputStream());
+				input = new DataInputStream(socket.getInputStream());
+			}
+			catch (IOException e) {}
+			while (running) {	
+				try {
+					print = input.readUTF();
+					Platform.runLater(new Runnable() {
+						public void run() {
+							gui.setOutput(print);
+						}
+					});
+				}
+				catch (IOException e) {
+					break;
+				}
+			}
+		}
 	}
-}
+
+	public boolean isRunning() {
+		return running;
+	}
+		
+/*
+
+		if (firstConnection) {
+				socket = serversocket.accept();
+				output = new DataOutputStream(socket.getOutputStream());
+				input = new DataInputStream(socket.getInputStream());
+				firstConnection = false;
+		} else if (!socket.getInetAddress().isReachable(500)) {
+				socket = serversocket.accept();
+				output = new DataOutputStream(socket.getOutputStream());
+				input = new DataInputStream(socket.getInputStream());
+		}
+		*/
+			//}
+			/*
+			byte[] buffer = new byte[1024];
+			int read;
+			String out = "";
+			while((read = input.read(buffer)) != -1) {
+				out += new String(buffer, 0, read);
+			};
+
+			*/
+			//return input.readUTF();
+	
+		//return out;
+	}
+
 
